05d41e1613ae7f94c739e3df677f613cb8fff8df
Tony <ca.njoku@yahoo.com>
2018-10-11 01:43:47 +0100

    test framework implemented

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,4 +1,34 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+const { interface, bytecode } = require('../compile')
+
+let accounts;
+let inbox;
+beforeEach(async()=>{
+  // get a list of all accounts
+  accounts = await web3.eth.getAccounts()
+
+  // use one of those to deploy the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ["tony", "njoku"] })
+    .send({ from: accounts[0], gas: '1000000' })
+});
+
+describe('Inbox', ()=>{
+  it('deploys a contract', ()=>{
+    assert.ok(inbox.options.address)
+  })
+
+  it('has a default name', async()=>{
+    const name = await inbox.methods.name().call();
+    assert.equal(name, 'tony')
+  })
+
+  it('has a default surname', async()=>{
+    const surname = await inbox.methods.surname().call();
+    assert.equal(surname, 'njoku')
+  })
+})
